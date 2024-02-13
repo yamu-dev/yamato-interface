fa17b40560f7a82fe3d4fcaa99e35afc0b586b6c
naizo10 <naizo018767@gmail.com>
2024-02-13 16:07:19 +0900

    Add useBalanceOf function in VotingEscrow hook

---
+++ hooks/VotingEscrow/useBalanceOf.ts
@@ -0,0 +1,25 @@
+import { useReadContract } from "wagmi";
+import VotingEscrowABI from "lib/constants/abis/VotingEscrow.json";
+
+export default function useBalanceOf(address?: `0x${string}`): {
+  readFn: ReturnType<
+    typeof useReadContract<typeof VotingEscrowABI, "balanceOf", [bigint]>
+  >;
+} {
+  const config = {
+    address: process.env.NEXT_PUBLIC_VE_ADDRESS as `0x${string}`,
+    abi: VotingEscrowABI,
+  };
+  const readFn = useReadContract<typeof VotingEscrowABI, "balanceOf", [bigint]>({
+    ...config,
+    functionName: "balanceOf",
+    args: [address],
+    query: {
+      enabled: !!address,
+    },
+  });
+
+  return {
+    readFn,
+  };
+}
