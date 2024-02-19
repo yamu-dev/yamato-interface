743849bafb78f7858794a9f7173987bef9ca1540
naizo10 <naizo018767@gmail.com>
2024-02-19 18:47:50 +0900

    Add DashboardPage with VotingEscrow component

---
+++ components/index.tsx
@@ -0,0 +1,42 @@
+import Router from "next/router";
+import {
+  Spinner,
+  Container,
+  Heading,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  Center,
+  Grid,
+} from "@chakra-ui/react";
+import { useAccount } from "wagmi";
+import Layout from "components/layouts/layout";
+import VotingEscrow from "components/votingEscrow/index";
+import { useTranslation } from "react-i18next";
+
+export default function DashboardPage() {
+  const { t } = useTranslation();
+  const account = useAccount();
+
+  return (
+    <Layout>
+      <Container maxW="container.xl" py={16}>
+        {/* <Heading size={"lg"}>{t("DASHBOARD")}</Heading> */}
+
+        <Center >
+          <Grid
+            bg="white"
+            templateColumns="repeat(1, 1fr)"
+            gap={8}
+            mt={{ base: 4, md: 8 }}
+            width="80%"
+          >
+            <VotingEscrow address={account.address} />
+          </Grid>
+        </Center>
+
+      </Container>
+    </Layout>
+  );
+}
