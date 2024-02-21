7f8500200245d86876b5904c00f1258baea303fb
naizo10 <naizo018767@gmail.com>
2024-02-21 12:36:46 +0900

    Add modal and localization to NewLockForm component

--- components/votingEscrow/NewLockForm.tsx
+++ components/votingEscrow/NewLockForm.tsx
@@ -1,14 +1,34 @@
-import { ButtonProps, Button } from "@chakra-ui/react";
+import { ButtonProps, Button, useDisclosure } from "@chakra-ui/react";
+import { LockType } from "lib/types/VotingEscrow";
+import { useTranslation } from "react-i18next";
+import FormModal from "./FormModal";
 
 export default function NewLockForm({
   address,
   ...props
 }: { address?: `0x${string}` } & ButtonProps) {
+  const { t } = useTranslation();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Button variant={"solid"} colorScheme="green" size={"sm"} {...props}>
-        ロックを作成
+      <Button
+        variant={"solid"}
+        colorScheme="green"
+        size={"sm"}
+        onClick={onOpen}
+        {...props}
+      >
+        {t("VE_CREATE_LOCK")}
       </Button>
+      {isOpen && (
+        <FormModal
+          address={address}
+          type={LockType.CREATE_LOCK}
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      )}
     </>
   );
 }
