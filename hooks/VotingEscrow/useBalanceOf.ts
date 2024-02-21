b0ef306a98a3f7ca98267205c129fe20bbe7966b
naizo10 <naizo018767@gmail.com>
2024-02-21 13:10:07 +0900

    Add useLocked hook and refactor useBalanceOf hook

--- hooks/VotingEscrow/useBalanceOf.ts
+++ hooks/VotingEscrow/useBalanceOf.ts
@@ -10,14 +10,16 @@ export default function useBalanceOf(address?: `0x${string}`): {
     address: process.env.NEXT_PUBLIC_VE_ADDRESS as `0x${string}`,
     abi: VotingEscrowABI,
   };
-  const readFn = useReadContract<typeof VotingEscrowABI, "balanceOf", [bigint]>({
-    ...config,
-    functionName: "balanceOf",
-    args: [address],
-    query: {
-      enabled: !!address,
+  const readFn = useReadContract<typeof VotingEscrowABI, "balanceOf", [bigint]>(
+    {
+      ...config,
+      functionName: "balanceOf",
+      args: [address],
+      query: {
+        enabled: !!address,
+      },
     },
-  });
+  );
 
   return {
     readFn,
