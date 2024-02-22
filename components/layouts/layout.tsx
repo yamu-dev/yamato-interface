fcb957dded11ebe3ccc83b7967a167ab4abcb781
naizo10 <naizo018767@gmail.com>
2024-02-22 20:22:52 +0900

    Add background image to layout component

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useContext, useEffect } from "react";
-
-import { Box, useColorMode } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Header from "./Header";
 import Footer from "./Footer";
 import MetaTags from "./MetaTags";
@@ -13,17 +11,13 @@ export default function Layout({
   title?: string;
   children: React.ReactNode;
 }) {
-  // Dark mode only for now
-  const { colorMode, toggleColorMode } = useColorMode();
-  useEffect(() => {
-    if (colorMode === "light") toggleColorMode();
-  }, [colorMode]);
-
   return (
     <>
       <MetaTags />
       <Header title={title ? title : "VotingEscrow"} />
-      <Box bg={"gray.800"}>{children}</Box>
+      <Box bgImage="url('/background_main.webp')">
+        {children}
+      </Box>
       <Footer />
     </>
   );
