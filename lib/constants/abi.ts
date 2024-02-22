b1dd911006253f05fba317250652ee8948939f46
naizo10 <naizo018767@gmail.com>
2024-02-22 21:38:18 +0900

    Update Yamato ABI imports and references

--- lib/constants/abi.ts
+++ lib/constants/abi.ts
@@ -1,10 +1,17 @@
+import FeePoolV2 from "lib/abis/yamato/FeePoolV2.json";
+import ScoreWeightController from "lib/abis/yamato/ScoreWeightController.json";
+import ScoreRegistry from "lib/abis/yamato/ScoreRegistry.json";
+import YmtMinter from "lib/abis/yamato/YmtMinter.json";
+import veYMT from "lib/abis/yamato/veYMT.json";
+import YMT from "lib/abis/yamato/YMT.json";
+
 export interface ISystemContractAbis {
-  FeeDistributor: string;
-  GaugeController: string;
-  Gauge: string;
-  Minter: string;
-  VotingEscrow: string;
-  ERC20: string;
+  FeeDistributor: any[];
+  GaugeController: any[];
+  Gauge: any[];
+  Minter: any[];
+  VotingEscrow: any[];
+  ERC20: any[];
 }
 
 export interface IVeSystemAbis {
@@ -16,20 +23,20 @@ export interface IVeSystemAbis {
 export const veSystemAbis: IVeSystemAbis = {
   yamato: {
     1: {
-      FeeDistributor: "",
-      GaugeController: "",
-      Gauge: "",
-      Minter: "",
-      VotingEscrow: "",
-      ERC20: "",
+      FeeDistributor: FeePoolV2,
+      GaugeController: ScoreWeightController,
+      Gauge: ScoreRegistry,
+      Minter: YmtMinter,
+      VotingEscrow: veYMT,
+      ERC20: YMT,
     },
     11155111: {
-      FeeDistributor: "lib/abis/yamato/FeeDistributor.json",
-      GaugeController: "lib/abis/yamato/GaugeController.json",
-      Gauge: "lib/abis/yamato/Gauge.json",
-      Minter: "lib/abis/yamato/Minter.json",
-      VotingEscrow: "lib/abis/yamato/VotingEscrow.json",
-      ERC20: "lib/abis/yamato/ERC20.json",
+      FeeDistributor: FeePoolV2,
+      GaugeController: ScoreWeightController,
+      Gauge: ScoreRegistry,
+      Minter: YmtMinter,
+      VotingEscrow: veYMT,
+      ERC20: YMT,
     },
   },
 };
