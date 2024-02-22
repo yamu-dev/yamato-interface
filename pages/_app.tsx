27ed7c1e2e24f7915bde6d2973381b68255157f0
naizo10 <naizo018767@gmail.com>
2024-02-22 20:52:37 +0900

    Update theme settings and enhance ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,33 @@ import "src/css/rsuite-override.css";
 import type { AppProps } from "next/app";
 import "lib/i18nConfig";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
-import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { State, WagmiProvider } from "wagmi";
+import { WagmiProvider } from "wagmi";
 import { config, projectId } from "lib/connector";
-
-import { sepolia, mainnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        color: "#818181",
+      },
+    },
+  },
+});
 
 const queryClient = new QueryClient();
 
 createWeb3Modal({
   wagmiConfig: config,
-  themeMode: "dark",
+  themeMode: "light",
   projectId: projectId,
   enableAnalytics: false,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
           <ColorModeScript initialColorMode={"light"} />
