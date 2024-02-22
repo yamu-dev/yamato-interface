f9eaf7302827573a59f9199aa61b8ce7ba12f364
naizo10 <naizo018767@gmail.com>
2024-02-22 17:01:53 +0900

    Refactor wagmiConfig setup in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,41 +3,20 @@ import type { AppProps } from "next/app";
 import "lib/i18nConfig";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { WagmiProvider } from "wagmi";
+import { State, WagmiProvider } from "wagmi";
+import { config, projectId } from "lib/connector";
+
 import { sepolia, mainnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
-// 0. Setup queryClient
 const queryClient = new QueryClient();
-
-// 1. Get projectId at https://cloud.walletconnect.com
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
-
-// 2. Create wagmiConfig
-const metadata = {
-  name: "Web3Modal",
-  description: "Web3Modal Example",
-  url: "https://web3modal.com", // origin must match your domain & subdomain
-  icons: ["https://avatars.githubusercontent.com/u/37784886"],
-};
-
-const chains = [mainnet, sepolia] as const;
-const config = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata,
-  enableWalletConnect: true,
-  enableInjected: true,
-  enableEIP6963: true,
-  enableCoinbase: true,
-});
 
 createWeb3Modal({
   wagmiConfig: config,
   themeMode: "dark",
-  projectId,
-  enableAnalytics: true,
+  projectId: projectId,
+  enableAnalytics: false, // Optional - defaults to your Cloud configuration
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
