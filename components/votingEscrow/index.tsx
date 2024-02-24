f220d442e742d1268a374eafcb0f2ca83181254d
naizo10 <naizo018767@gmail.com>
2024-02-24 16:46:08 +0900

    Refactor votingEscrow component, remove Tooltip and Text

--- components/votingEscrow/index.tsx
+++ components/votingEscrow/index.tsx
@@ -3,11 +3,8 @@ import {
   CardHeader,
   CardBody,
   Heading,
-  Tooltip,
   Divider,
-  Text,
 } from "@chakra-ui/react";
-import { QuestionIcon } from "@chakra-ui/icons";
 import { useTranslation } from "react-i18next";
 import LockStats from "components/votingEscrow/LockStats";
 import Reward from "components/votingEscrow/Rewards";
@@ -28,22 +25,10 @@ export default function VotingEscrow({ address }: { address?: `0x${string}` }) {
     >
       <CardHeader bg={"#f9aea5"} py={2}>
         <Heading size="md" color={"white"}>
-          My Information
+          My YMT Portfolio
         </Heading>
       </CardHeader>
       <CardBody>
-        <Heading fontSize={"xl"} py={2}>
-          {t("VE_YMT")}
-          <Tooltip
-            hasArrow
-            label={
-              <Text whiteSpace={"pre-wrap"}>{t("VE_YMT_REWARD_HELP")}</Text>
-            }
-          >
-            <QuestionIcon fontSize={"lg"} mb={1} ml={1} cursor="help" />
-          </Tooltip>
-        </Heading>
-        <Divider variant="dashed" mt={4} mb={4} />
         <LockStats address={address} />
         <Divider variant="dashed" py={2} />
         <Reward address={address} />
