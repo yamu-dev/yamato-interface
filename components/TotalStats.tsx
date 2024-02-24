9bb7a671792714d49e286e2912680b3467c5a1c0
naizo10 <naizo018767@gmail.com>
2024-02-24 16:32:18 +0900

    Update TotalStats component and add logo image

--- components/TotalStats.tsx
+++ components/TotalStats.tsx
@@ -5,7 +5,10 @@ import {
   Heading,
   Tooltip,
   Divider,
+  HStack,
+  chakra,
   Text,
+  Image,
 } from "@chakra-ui/react";
 import { QuestionIcon } from "@chakra-ui/icons";
 import { useTranslation } from "react-i18next";
@@ -26,10 +29,52 @@ export default function VotingEscrow({ address }: { address?: `0x${string}` }) {
     >
       <CardHeader bg={"#5bad92"} py={2}>
         <Heading size="md" color={"white"}>
-          Total Information
+          Total YMT Overview
         </Heading>
       </CardHeader>
       <CardBody>
+        <Heading fontSize={"xl"} pb={1}>
+          <HStack>
+            <Image src="/logo192.png" alt="Logo" boxSize="24px" />
+            <Text>{t("VE_YMT")}</Text>
+            <Tooltip
+              hasArrow
+              label={
+                <Text whiteSpace={"pre-wrap"}>{t("VE_YMT_REWARD_HELP")}</Text>
+              }
+            >
+              <QuestionIcon fontSize={"lg"} cursor="help" />
+            </Tooltip>
+          </HStack>
+        </Heading>
+        <Divider variant="dashed" my={2} />
+        <HStack justifyContent={"space-between"}>
+          <chakra.p>{t("TOTAL_YMT_VOTE_LOCKED")}</chakra.p>
+          <chakra.p fontSize={"2xl"}>
+            <>{"0.0"}</>
+            <chakra.span fontSize={"lg"} ml={1}>
+              YMT
+            </chakra.span>
+          </chakra.p>
+        </HStack>
+        <HStack justifyContent={"space-between"}>
+          <chakra.p>{t("PERCENTAGE_YMT_LOCKED")}</chakra.p>
+          <chakra.p fontSize={"2xl"}>
+            <>{"0.0"}</>
+            <chakra.span fontSize={"lg"} ml={1}>
+              %
+            </chakra.span>
+          </chakra.p>
+        </HStack>
+        <HStack justifyContent={"space-between"}>
+          <chakra.p>{t("TOTAL_VE_YMT")}</chakra.p>
+          <chakra.p fontSize={"2xl"}>
+            <>{"0.0"}</>
+            <chakra.span fontSize={"lg"} ml={1}>
+              veYMT
+            </chakra.span>
+          </chakra.p>
+        </HStack>
       </CardBody>
     </Card>
   );
