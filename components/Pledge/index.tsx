6b6f788e57ef1400fbf4533c3301005fbeede7a4
m.k <m.kasahara.develop@gmail.com>
2024-03-25 09:42:53 +0900

    pertical commit

--- components/Pledge/index.tsx
+++ components/Pledge/index.tsx
@@ -1,3 +1,4 @@
+import { Card, CardBody, CardHeader, Heading, Tooltip } from '@chakra-ui/react';
 import { CategoryTitle, ConentBox, HeaderBox2 } from '../CommonItem';
 import Collateral from './Collateral';
 import Debt from './Debt';
@@ -5,13 +6,27 @@ import Debt from './Debt';
 export default function Pledge() {
   return (
     <>
-      <HeaderBox2>
-        <CategoryTitle>My Pledge</CategoryTitle>
-      </HeaderBox2>
-      <ConentBox>
-        <Collateral />
-        <Debt />
-      </ConentBox>
+      <Card
+        flex={1}
+        maxW="5xl"
+        bg={"#fcfaf2"}
+        style={{
+          boxShadow: "rgba(0, 0, 0, 0.25) 3px 3px 0px",
+          borderRadius: "0px",
+        }}
+        color={"#818181"}
+      >
+        {/* <CardHeader bg={"#F9AEA5"} py={2}> */}
+        <CardHeader bg={"#5bad92"} py={2}>
+          <Heading size="md" color={"white"}>
+            My Pledge
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Collateral />
+          <Debt />
+        </CardBody>
+      </Card>
     </>
   );
 }
